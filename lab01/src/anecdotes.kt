bd0283f63ce2be5cfaa5901b1452b3ab3b234ef1
trvehazzk3r <dimakrok@bk.ru>
2021-09-15 00:16:52 +0300

    ADD: ready for installer version

--- lab01/src/anecdotes.kt
+++ lab01/src/anecdotes.kt
@@ -1,10 +1,6 @@
 import java.awt.Font
-import java.awt.event.ActionListener
 import java.io.BufferedReader
-import java.io.File
-import java.io.IOException
 import java.io.InputStreamReader
-import java.util.concurrent.TimeUnit
 import javax.swing.*
 import kotlin.random.Random
 
