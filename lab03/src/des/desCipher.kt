37b6eb0b35a04b52ae1cccd2488704443cf9d6f7
trvehazzk3r <dimakrok@bk.ru>
2021-10-15 17:07:42 +0300

    ADD: fucking permutations

--- lab03/src/des/desCipher.kt
+++ lab03/src/des/desCipher.kt
@@ -1,17 +1,50 @@
 package des
 
 // Add zeroes until 64 bits and add a number of added bytes
-fun prepareWorkBytes(bytes: List<Byte>): List<Byte>
+fun prepareWorkBlocks(bytes: List<Byte>): List<Long>
 {
     val workBytes = bytes.toMutableList()
-    val addedZeroes = (workBytes.size % 8).toByte()
-    for (i in 0 until addedZeroes)
+    val addedZeroes: Byte = if (workBytes.size >= 8)
+        (workBytes.size % 8).toByte()
+    else
+        (8 - workBytes.size).toByte()
+    for (i in 0 until addedZeroes + 7)
     {
         workBytes.add(0)
     }
-
     workBytes.add(addedZeroes)
-    return workBytes
+
+    val outBlocks = MutableList<Long>(workBytes.size / 8) { _ -> 0 }
+
+    // God, save me from my cycles...
+    var curBlockNum = 0
+    var passedBytes = 0
+    for (i in workBytes.indices)
+    {
+        outBlocks[curBlockNum] = (outBlocks[curBlockNum] shl 8) or workBytes[i].toLong()
+        passedBytes++
+        if (passedBytes == 7)
+        {
+            passedBytes = 0
+            curBlockNum++
+        }
+    }
+
+    return outBlocks
+}
+
+fun makeBlocksToOneList(byteBlocks: List<List<Byte>>): List<Byte>
+{
+    val outList = mutableListOf<Byte>()
+    for (block in byteBlocks)
+    {
+        for (byte in block)
+        {
+            outList.add(byte)
+        }
+    }
+
+    return outList
 }
 
 fun expandKey(key: List<Byte>): List<Long>
@@ -21,20 +54,41 @@ fun expandKey(key: List<Byte>): List<Long>
     return keys48bit
 }
 
-fun cipher(bytesToCipher: List<Byte>, key: List<Byte>): List<Byte>
+fun makePermutationWith(block: Long, table: List<Byte>): Long
+{
+    var newBlock: Long = 0
+    for (i in 0 until 64)
+    {
+        newBlock = newBlock or ((block shr (64 - table[i])) and 0x01) shl (63 - i)
+    }
+    return newBlock
+}
+
+fun makeInitialPermutationAndDivide(block: Long): Pair<Int, Int>
+{
+    val newBlock = makePermutationWith(block, DESTables.ipTable)
+
+    return Pair((newBlock shr 32).toInt(), newBlock.toInt())
+}
+
+fun makeFinalPermutationAndMakeBlock(left: Int, right: Int): Long =
+    makePermutationWith((left.toLong() shl 32) or right.toLong(), DESTables.fpTable)
+
+fun cipher(bytesToCipher: List<Byte>, key: List<Byte>): List<Long>
 {
     if (key.size != 8)
     {
         throw InvalidKeyException("Key is invalid: we need 8 bytes in it")
     }
 
-    val workBytes = prepareWorkBytes(bytesToCipher)
+    val workBlocks = prepareWorkBlocks(bytesToCipher)
 
     val keys48bit = expandKey(key)
 
-    val curLeftPart: Int // 32 bit
-    val curRightPart: Int // 32 bit
+    for (block in workBlocks)
+    {
+        val (curLeft, curRight) = makeInitialPermutationAndDivide(block)
+    }
 
-
-    return workBytes
+    return workBlocks
 }
