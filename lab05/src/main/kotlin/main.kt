1a27ab05cd2ad50eb5373f7c9914e6debffcf061
trvehazzk3r <dimakrok@bk.ru>
2021-12-02 12:59:01 +0300

    ADD: ready to go

--- lab05/src/main/kotlin/main.kt
+++ lab05/src/main/kotlin/main.kt
@@ -1,20 +1,68 @@
 import java.io.File
 import java.security.*
+import java.security.spec.PKCS8EncodedKeySpec
 import java.security.spec.X509EncodedKeySpec
 
-const val privateKeyFilename = "privatekey.json"
-const val publicKeyFilename = "publickey.json"
+const val privateKeyFilename = "privatekey"
+const val publicKeyFilename = "publickey"
+const val signatureFilename = "signature"
+
+object KeyHelper {
+    private val keyFactory by lazy { KeyFactory.getInstance("RSA") }
+
+    fun getPublicKey(encodedPublic: ByteArray): PublicKey {
+        val keySpec = X509EncodedKeySpec(encodedPublic)
+        return keyFactory.generatePublic(keySpec)
+    }
+
+    fun getPrivateKey(encodedPrivate: ByteArray): PrivateKey {
+        val keySpec = PKCS8EncodedKeySpec(encodedPrivate)
+        return keyFactory.generatePrivate(keySpec)
+    }
+}
+
+fun getEncodedKey(keyFile: File): ByteArray {
+    return keyFile.readBytes()
+}
+
+fun getPublicKey(): PublicKey {
+    val encodedPublic = getEncodedKey(File(publicKeyFilename))
+    return KeyHelper.getPublicKey(encodedPublic)
+}
+
+fun getPrivateKey(): PrivateKey {
+    val encodedPrivate = getEncodedKey(File(privateKeyFilename))
+    return KeyHelper.getPrivateKey(encodedPrivate)
+}
 
 fun generateKeys() {
     val generator = KeyPairGenerator.getInstance("RSA")
     val keys: KeyPair = generator.generateKeyPair()
 
-    keys.public.encoded.forEach { print(it) }
-    val keyFactory = KeyFactory.getInstance("RSA")
-    val keySpec = X509EncodedKeySpec(keys.public.encoded)
-    println()
-    keyFactory.generatePublic(keySpec).encoded.forEach { print(it) }
+//    keys.public.encoded.forEach { print(it) }
+//    println()
+//    KeyHelper.getPublicKey(keys.public.encoded).encoded.forEach { print(it) }
 
+    File(publicKeyFilename).writeBytes(keys.public.encoded)
+    File(privateKeyFilename).writeBytes(keys.private.encoded)
+}
+
+fun signFile(filename: String) {
+    val signature = Signature.getInstance("SHA256withRSA")
+    signature.initSign(getPrivateKey())
+    signature.update(File(filename).readBytes())
+    File(signatureFilename).writeBytes(signature.sign())
+}
+
+fun verifyDigitalSignature(filename: String) {
+    val signature = Signature.getInstance("SHA256withRSA")
+    signature.initVerify(getPublicKey())
+    signature.update(File(filename).readBytes())
+
+    if (signature.verify(File(signatureFilename).readBytes()))
+        println("It's ok")
+    else
+        println("File corrupted, don't open it o.o")
 }
 
 fun main(args: Array<String>) {
@@ -33,9 +81,15 @@ fun main(args: Array<String>) {
         return
     }
 
+    if (mode != "keygen" && args.size < 2) {
+        println("File name to be signed or verified is needed.")
+
+        return
+    }
+
     when (mode) {
         "keygen" -> generateKeys()
-        "sign" -> TODO()
-        "verify" -> TODO()
+        "sign" -> signFile(args[1])
+        "verify" -> verifyDigitalSignature(args[1])
     }
 }
